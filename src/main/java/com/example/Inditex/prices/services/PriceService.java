9077a6221ac731f56e741a0a4e1f916a90cb13c5
Gabriel Lorenzatti <gabriellorenzatti94@gmail.com>
2021-07-15 23:04:49 -0300

    "add service and repository"

--- src/main/java/com/example/Inditex/prices/services/PriceService.java
+++ src/main/java/com/example/Inditex/prices/services/PriceService.java
@@ -2,14 +2,18 @@ package com.example.Inditex.prices.services;
 
 import com.example.Inditex.prices.dto.PriceDto;
 import com.example.Inditex.prices.dto.PriceIncomingDto;
+import com.example.Inditex.prices.repository.PriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PriceService implements Price {
 
+    private final PriceRepository priceRepository;
+
     @Autowired
-    public PriceService() {
+    public PriceService(PriceRepository priceRepository) {
+        this.priceRepository = priceRepository;
     }
 
     public PriceDto savePrice(PriceIncomingDto priceIncomingDto) {
