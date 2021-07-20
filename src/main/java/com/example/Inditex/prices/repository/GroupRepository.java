0a058d1680e125033b897ef1243c1d822ec76e67
Gabriel Lorenzatti <gabriellorenzatti94@gmail.com>
2021-07-19 23:44:15 -0300

    refactor for crud and rename

--- src/main/java/com/example/Inditex/prices/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Inditex.prices.repository;
-
-import com.example.Inditex.prices.model.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Brand, Long> {
-}
