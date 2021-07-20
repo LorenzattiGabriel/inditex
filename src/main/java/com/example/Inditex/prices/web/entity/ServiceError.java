b788a22d390d42297b8bdc36dc1eecc03af785de
Gabriel Lorenzatti <gabriellorenzatti94@gmail.com>
2021-07-19 23:43:36 -0300

    add BusinessRulesError

--- src/main/java/com/example/Inditex/prices/web/entity/ServiceError.java
+++ src/main/java/com/example/Inditex/prices/web/entity/ServiceError.java
@@ -4,6 +4,7 @@ package com.example.Inditex.prices.web.entity;
 public enum ServiceError {
 
     BUSINESS_BRAND_NOT_FOUND(404, "Brand  not found"),
+    BUSINESS_PRICE_NOT_FOUND(404, "Brand  not found"),
     BUSINESS_PRODUCT_NOT_FOUND(404, "Product  not found"),
     BUSINESS_CURRENCY_NOT_FOUND(404, "Currency  not found"),
     TECHNICAL_ERROR(500, "Technical Error"),
